4debeeb79320ca7d6a896e34198bb46154f3d710
Mat <36236271+mamet8@users.noreply.github.com>
2021-03-02 11:19:54 +0700

    Update main.js

--- main.js
+++ main.js
@@ -89,7 +89,6 @@ async function printLogs(msg){
 
 event.on('message-new', async(chat) =>{
     const owner = ["6282260888474@s.whatsapp.net","6281396061030@s.whatsapp.net", event.user.jid]
-    console.log("NEW MESSAGE")
     const msg = wa.serialize(chat)
     const text = msg.text
     const cmd = text.toLowerCase()
