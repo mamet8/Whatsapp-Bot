c77abd90732969163097d66ed22084d546e2873b
Mat <36236271+mamet8@users.noreply.github.com>
2021-03-26 00:25:07 +0700

    Update helper.js

--- mat/helper.js
+++ mat/helper.js
@@ -429,3 +429,41 @@ exports.sendReplyWA = function(to, text, prevtext, mids=[]){
     ini = m.key.id
     wa.sendMessage(to, text, MessageType.extendedText, { contextInfo: {"mentionedJid": mids,"stanzaId": ini, "participant": waid, "quotedMessage": { "conversation": prevtext}} })
 }
+
+exports.addMetadata = function(packname, author) {    
+    let name = `${author}_${packname}`
+    if (fs.existsSync(`./media/sticker/${name}.exif`)) return `./media/sticker/${name}.exif`
+    const json = {  
+        "sticker-pack-name": packname,
+        "sticker-pack-publisher": author,
+    }
+    const x = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00])  
+    const bytes = [0x00, 0x00, 0x16, 0x00, 0x00, 0x00]  
+
+    let len = JSON.stringify(json).length   
+    let last
+
+    if (len > 256) {
+        len = len - 256 
+        bytes.unshift(0x01) 
+    } else {
+        bytes.unshift(0x00) 
+    }   
+
+    if (len < 16) { 
+        last = len.toString(16) 
+        last = "0" + len
+    } else {    
+        last = len.toString(16) 
+    }   
+
+    const xx = Buffer.from(last, "hex")
+    const xxx = Buffer.from(bytes) 
+    const xxxx = Buffer.from(JSON.stringify(json))
+
+    const buffer = Buffer.concat([x, xx, xxx, xxxx])  
+    fs.writeFile(`./media/sticker/${name}.exif`, buffer, (err) => {  
+        return `./media/sticker/${name}.exif`
+    })
+
+}
